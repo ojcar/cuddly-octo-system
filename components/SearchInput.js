9117cdfbecba42ba42ea3c7b7d57b9f2dfe98fc6
Oscar Rodriguez <vega@blogactivo.com>
2018-02-03 16:53:14 -0800

    connect searchInput to App component

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -2,15 +2,41 @@ import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 
 export default class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChangeText = (text) => {
+    this.setState({ text: text })
+  }
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+
+    onSubmit(text);
+    this.setState({text: ''});
+  }
+
   render() {
+    const { placeholder } = this.props;
+    const { text } = this.state;
+
     return (
       <View style={styles.container}>
         <TextInput
           autocorrect={false}
-          placeholder={this.props.placeholder}
+          placeholder={placeholder}
           placeholterTextColor="white"
           style={styles.textInput}
           clearButtonMode="always"
+          onChangeText={this.handleChangeText}
+          onSubmitEditing={this.handleSubmitEditing}
         />
       </View>
     );
