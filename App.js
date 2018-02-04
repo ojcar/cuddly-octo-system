61a883eccda782b093413a359cb62abcb67a75d3
Oscar Rodriguez <vega@blogactivo.com>
2018-02-03 16:29:24 -0800

    adding a background

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, Text, TextInput, View, ImageBackground, KeyboardAvoidingView, Platform } from 'react-native';
 
+import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container}>
-        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-        <Text style={[styles.smallText, styles.textStyle]}>Cloudy</Text>
-        <Text style={[styles.largeText, styles.textStyle]}>24 C</Text>
+        <ImageBackground
+          source={getImageForWeather('clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
 
-        <SearchInput placeholder="Search any city" />
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+            <Text style={[styles.smallText, styles.textStyle]}>Cloudy</Text>
+            <Text style={[styles.largeText, styles.textStyle]}>24 C</Text>
+
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
@@ -20,9 +30,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover'
   },
   largeText: {
     fontSize: 44
@@ -39,7 +56,8 @@ const styles = StyleSheet.create({
       android: {
         fontFamily: 'Roboto',
       }
-    })
+    }),
+    color: 'white'
   },
   textInput: {
     backgroundColor: '#666',
@@ -51,4 +69,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     alignSelf: 'center',
   },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20
+  }
 });
